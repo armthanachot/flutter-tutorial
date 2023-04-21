4fd933d2d74939ae848e7c77720e0463c10d2088
Thanachot Tesjaroen <arm1997a@gmail.com>
2023-04-21 11:42:18 +0700

    commit

---
+++ lib/widget/foodMenu.dart
@@ -0,0 +1,6 @@
+class FoodMenu {
+  String name;
+  double price;
+  String imageUrl;
+  FoodMenu({required this.name, required this.price, required this.imageUrl});
+}
--- lib/widget/stateful/homepage.dart
+++ lib/widget/stateful/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:mybasicapp/widget/foodMenu.dart';
 import 'package:mybasicapp/widget/myAppImg.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -12,41 +13,59 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // สร้าง state
-  int lastIndex = 0;
-  List<Widget> more_display = [];
 
   // แสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
-    List<Widget> display = [];
-    // สร้างกลุ่มข้อมูล
-    for (int i = 0; i < 10; i++) {
-      display.add(Text("Row $i", style: TextStyle(fontSize: 20)));
-      lastIndex++;
-    }
-
-    display.addAll(more_display);
+    List<FoodMenu> menus = [];
+    menus.add(FoodMenu(
+        name: "ข้าวผัดปู",
+        price: 150,
+        imageUrl:
+            "https://images.unsplash.com/photo-1637759079728-3f900db7a782?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3JhYiUyMGZyaWVkJTIwcmljZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"));
+    menus.add(FoodMenu(
+        name: "ข้าวผัดกุ้ง",
+        price: 100,
+        imageUrl:
+            "https://images.unsplash.com/photo-1665199020996-66cfdf8cba00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNocmltcCUyMGZyaWVkJTIwcmljZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"));
+    menus.add(FoodMenu(
+        name: "ข้าวผัดหมู",
+        price: 50,
+        imageUrl:
+            "https://media.istockphoto.com/id/1416030500/photo/close-up-fried-rice-with-pork-and-vegetables.jpg?b=1&s=170667a&w=0&k=20&c=mgp51pqjzwc-MB90gf3vwjH6c7yWJZE6p3Pg4a9ucwA="));
     return Scaffold(
       appBar: AppBar(
         title: Text("My App"),
       ),
       body: Center(
-        child: ListView( // เมื่อ data เกินหน้าจอ ก็สามารถ scroll ดูได้ down up
-          children: display,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getMoreList,
-        child: Icon(Icons.more),
+        child: ListView.builder(
+            itemCount: menus
+                .length, // ถ้าระบุตรงนี้คือจำกัดขนาดของ list ควรใช้เมื่อมากกว่า 10
+            itemBuilder: (BuildContext context, int index) {
+              //สร้างรายการที่ไม่จำกัดในตอนเริ่ม ทำให้ตอน scroll จะได้รายการขึ้นมาเรื่อยๆ เหมาะสำหรับกลุ่มข้อมูลขนาดใหญ่
+              return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ListTile(
+                      leading: MyAppImg(
+                          imageUrl: menus[index].imageUrl,
+                          width: 100,
+                          height: 100),
+                      title: Text("${menus[index].name}"),
+                      subtitle: Text("ราคา: ${menus[index].price} บาท"),
+                      onTap: () => {
+                        print(menus[index].name),
+                        print(menus[index].price),
+                      },
+                    ),
+                    Divider(
+                      height: 1,
+                      thickness: 1,
+                      color: Colors.grey[400],
+                    )
+                  ]);
+            }),
       ),
     );
-  }
-
-  void getMoreList() {
-    for (int i = lastIndex; i < lastIndex + 10; i++) {
-      setState(() {
-        more_display.add(Text("Row $i", style: TextStyle(fontSize: 20),));
-      });
-    }
   }
 }
